50f6b12072ea5093b257de0cd11d7890dfd08207
carlosbernat <69471013+carlosbernat@users.noreply.github.com>
2023-10-09 18:21:55 -0500

    Challenge1

---
+++ src/FirstApp.jsx
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react';
+import './App.css';
+
+
+
+const FirstApp = () => {
+  return (
+    <div>
+      <h1>Contador</h1>
+      <span>10</span>
+    </div>
+  )
+}
+
+
+export default FirstApp;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
